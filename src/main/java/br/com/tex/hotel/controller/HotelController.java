2b444662dc7cd9d51fe9ba742b28ac8aae681256
Willian73 <will73tur@gmail.com>
2023-04-15 10:20:25 -0300

    Criando CRUD do hotel em nossa API Refatorando código com práticas melhores

--- src/main/java/br/com/tex/hotel/controller/HotelController.java
+++ src/main/java/br/com/tex/hotel/controller/HotelController.java
@@ -1,81 +1,54 @@
 package br.com.tex.hotel.controller;
 
 import br.com.tex.hotel.dao.HotelDAO;
-import br.com.tex.hotel.enums.Estado;
-import br.com.tex.hotel.enums.TipoLogradouro;
 import br.com.tex.hotel.model.dto.HotelInputAlteraDTO;
 import br.com.tex.hotel.model.dto.HotelInputDTO;
 import br.com.tex.hotel.model.dto.HotelOutputDetalheDTO;
+import br.com.tex.hotel.model.dto.RespostaListaHotelDTO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 /**
  * @author Willian
  */
-@Controller
-@RequestMapping("/hotel")
+@RestController
+@RequestMapping("/hoteis")
 public class HotelController {
 
     @Autowired
     private HotelDAO dao;
 
-    @GetMapping("/lista")
-    public String listHoteis(Model model){
-        model.addAttribute("list", dao.listAllHotel());
-        return "listHoteis";
+    @GetMapping
+    public RespostaListaHotelDTO listHoteis() {
+        return new RespostaListaHotelDTO(dao.listAllHotel());
     }
 
-    @GetMapping("/formulario")
-    public String formulario(Model model){
-        model.addAttribute("tipoLogradouros", TipoLogradouro.values());
-        model.addAttribute("estados", Estado.values());
-
-        return "formularioHotel";
+    @GetMapping("/{id}")
+    public HotelOutputDetalheDTO mostrarDetalhe(@PathVariable("id") Integer id) {
+        return new HotelOutputDetalheDTO(dao.getById(id));
     }
 
-    @PostMapping("/cadastra")
-    public String cadstra(HotelInputDTO hotelInputDTO, RedirectAttributes redirectAttributes){
-
+    @Transactional
+    @PostMapping
+    public ResponseEntity cadastra(@RequestBody HotelInputDTO hotelInputDTO) {
         dao.inserir(hotelInputDTO.toHotel(hotelInputDTO));
-        redirectAttributes.addFlashAttribute("sucess", "Hotel cadastrado com sucesso!");
-        return "redirect:/hotel/lista";
-    }
-
-    @GetMapping("/detalhe")
-    public String mostrarDetalhe(@RequestParam("id") Integer id, Model model){
-        model.addAttribute("hotel", new HotelOutputDetalheDTO(dao.getById(id)));
-        return "detalheHotel";
+        return ResponseEntity.status(HttpStatus.CREATED).body(hotelInputDTO);
     }
 
     @Transactional
-    @GetMapping("/deleta/{id}")
-    public String deleta(@PathVariable Integer id, RedirectAttributes redirectAttributes){
+    @DeleteMapping("/{id}")
+    public String deleta(@PathVariable Integer id) {
         dao.delete(dao.getById(id));
-        redirectAttributes.addFlashAttribute("sucess", "Hotel excluído com sucesso!");
-        return "redirect:/hotel/lista";
-    }
-
-    @GetMapping("/preparaAlterar/{id}")
-    public String preparaAlterar(@PathVariable Integer id, Model model){
-        model.addAttribute("hotel", dao.getById(id));
-        model.addAttribute("tipoLogradouros", TipoLogradouro.values());
-        model.addAttribute("estados", Estado.values());
-
-        return "formAlteraHotel";
+        return "Excluído com sucesso!";
     }
 
     @Transactional
-    @PostMapping("/altera")
-    public String altera(HotelInputAlteraDTO dto, RedirectAttributes redirectAttributes){
-        System.out.println(dto);
+    @PutMapping
+    public ResponseEntity altera(@RequestBody HotelInputAlteraDTO dto) {
         dao.alterar(dao.getById(dto.getId()), dto);
-
-        redirectAttributes.addFlashAttribute("sucess", "Hotel alterado com sucesso!");
-
-        return "redirect:/hotel/lista";
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(dto);
     }
 }
