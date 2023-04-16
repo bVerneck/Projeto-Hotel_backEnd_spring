39705805959528cb3e1fcca4d7c31c54fdf391d7
Andre Sampaio <andresampaio37@outlook.com>
2023-04-16 15:28:44 -0300

    Inserção crd cliente

--- src/main/java/br/com/tex/hotel/controller/ClienteController.java
+++ src/main/java/br/com/tex/hotel/controller/ClienteController.java
@@ -1,23 +1,45 @@
 package br.com.tex.hotel.controller;
 
 import br.com.tex.hotel.dao.ClienteDAO;
+import br.com.tex.hotel.model.dto.cliente.ClienteInputSalvatDTO;
+import br.com.tex.hotel.model.dto.cliente.ClienteOutputDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.List;
+
+@RestController
 @RequestMapping("/clientes")
 public class ClienteController {
     @Autowired
     private ClienteDAO clienteDAO;
 
-    @GetMapping("/lista")
-    public String clientes(Model model){
-        model.addAttribute("list", clienteDAO.listAllCliente());
-
-        return "listCliente";
+    @PostMapping
+    @Transactional
+    public ResponseEntity salvar(@RequestBody ClienteInputSalvatDTO dto) {
+        this.clienteDAO.inserir(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(dto);
     }
 
+    @GetMapping
+    public List<ClienteOutputDTO> lista() {
+       return this.clienteDAO.listAllCliente();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity getById(@PathVariable Integer id) {
+        return ResponseEntity.ok(this.clienteDAO.getById(id));
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public ResponseEntity excluir(@PathVariable Integer id) {
+        this.clienteDAO.delete(id);
+        return ResponseEntity.ok().build();
+    }
 }
--- src/main/java/br/com/tex/hotel/dao/ClienteDAO.java
+++ src/main/java/br/com/tex/hotel/dao/ClienteDAO.java
@@ -10,125 +10,41 @@ import java.util.ArrayList;
 import java.util.List;
 
 import br.com.tex.hotel.base.FactoryConnetion;
+import br.com.tex.hotel.model.dto.cliente.ClienteInputSalvatDTO;
+import br.com.tex.hotel.model.dto.cliente.ClienteOutputDTO;
 import br.com.tex.hotel.model.entitys.Cliente;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class ClienteDAO {
 
-	public Integer inserir(Cliente cliente) throws SQLException {
+    @PersistenceContext
+    private EntityManager em;
 
-		Connection conexao = FactoryConnetion.getConnection();
+    public void inserir(ClienteInputSalvatDTO dto) {
+       this.em.persist(dto.toEntityCliente());
+    }
 
-		String sql = "INSERT INTO cliente (nome, cpf, dataNascimento,"
-				+ " contato_id_contato, endereco_id_endereco, reserva_id_reserva)" + " VALUES(?, ?, ?, ?, ?, ?)";
-		PreparedStatement statement = conexao.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+    public void alterar() {}
 
-		statement.setString(1, cliente.getNome());
-		statement.setString(2, cliente.getCpf());
-		statement.setDate(3, Date.valueOf(cliente.getDataNascimento()));
-		statement.setInt(4, cliente.getContato().getId());
-		statement.setInt(5, cliente.getEndereco().getId());
-		statement.setInt(6, cliente.getReservas().get(0).getId()); // precisa modificar.
-
-		statement.executeUpdate();
-
-		ResultSet rs = statement.getGeneratedKeys();
-
-		int ultimoId = 0;
-		while (rs.next()) {
-			ultimoId = rs.getInt(1);
-		}
-
-		rs.close();
-		statement.close();
-		conexao.close();
-
-		return ultimoId;
+	public ClienteOutputDTO getById(Integer id) {
+		return new ClienteOutputDTO(this.em.find(Cliente.class, id));
 	}
 
-	public void alterar(Cliente cliente) throws SQLException {
-		Connection conexao = FactoryConnetion.getConnection();
-		String sql = "UPDATE funcionario SET matricula= ?, nome= ?, cpf= ?, salario= ?, dataNascimento= ?,"
-				+ " hotel_id_hotel= ?, endereco_id_endereco= ?, contato_id_contato= ? WHERE id_funcionario= ?";
 
-		PreparedStatement statement = conexao.prepareStatement(sql);
-
-		statement.setString(1, cliente.getNome());
-		statement.setString(2, cliente.getCpf());
-		statement.setDate(3, Date.valueOf(cliente.getDataNascimento()));
-		statement.setInt(4, cliente.getContato().getId());
-		statement.setInt(5, cliente.getEndereco().getId());
-		statement.setInt(6, cliente.getReservas().get(0).getId()); // precisa modificar.
-
-		statement.execute();
-
-		statement.close();
-		conexao.close();
-	}
-
-	public void delete(Cliente cliente) throws SQLException {
-		Connection conexao = FactoryConnetion.getConnection();
-		String sql = "DELETE FROM cliente WHERE id_cliente=?";
-
-		PreparedStatement statement = conexao.prepareStatement(sql);
-
-		statement.setInt(1, cliente.getId());
-		statement.execute();
-
-		statement.close();
-		conexao.close();
-	}
-
-	public Cliente getById(Integer id) throws SQLException {
-		Connection conexao = FactoryConnetion.getConnection();
-		String sql = "SELECT * from cliente WHERE id_cliente=?";
-		PreparedStatement statement = conexao.prepareStatement(sql);
-		statement.setInt(1, id);
-
-		ResultSet rs = statement.executeQuery();
-
-		Cliente cliente = null;
-
-		while (rs.next()) {
-			cliente = new Cliente(rs.getString("nome"), rs.getString("cpf"), rs.getDate("dataNascimento").toLocalDate(),
-					new ContatoDAO().getById(rs.getInt("contato_id_contato")),
-					new EnderecoDAO().getById(rs.getInt("endereco_id_endereco")));
-		}
-
-		rs.close();
-		statement.close();
-		conexao.close();
-
-		return cliente;
-	}
-
-	public List<Cliente> listAllCliente() {
-		List<Cliente> clientes = new ArrayList<>();
-		try{
-			Connection conexao = FactoryConnetion.getConnection();
-			String sql = "SELECT * from cliente";
-			PreparedStatement statement = conexao.prepareStatement(sql);
-
-			ResultSet rs = statement.executeQuery();
+    public void delete(Integer id) {
+       this.em.remove(this.em.find(Cliente.class, id));
+    }
 
 
-			while (rs.next()) {
-				Cliente cliente = new Cliente(rs.getInt("id_cliente"), rs.getString("nome"), rs.getString("cpf"),
-						rs.getDate("dataNascimento").toLocalDate(),
-						new ContatoDAO().getById(rs.getInt("contato_id_contato")),
-						new EnderecoDAO().getById(rs.getInt("endereco_id_endereco")));
-
-				clientes.add(cliente);
-			}
-
-			rs.close();
-			statement.close();
-			conexao.close();
-		}catch (Exception e){
-			e.printStackTrace();
-		}
-
-		return clientes;
+    public List<ClienteOutputDTO> listAllCliente() {
+		String jpql = "select c from Cliente c";
+		return this.em.createQuery(jpql, Cliente.class).getResultList()
+				.stream()
+				.map(cliente -> new ClienteOutputDTO(cliente))
+				.toList();
 	}
 }
